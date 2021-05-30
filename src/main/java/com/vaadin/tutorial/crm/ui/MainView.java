257ba781b01e7d89d594c5815c3716c6cc3c9d30
vahid <vahid.rafiei@gmail.com>
2021-05-30 18:24:42 +0200

    Populating the grid of Contacts.

--- src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
@@ -1,13 +1,33 @@
 package com.vaadin.tutorial.crm.ui;
 
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import com.vaadin.tutorial.crm.backend.entity.Contact;
+import com.vaadin.tutorial.crm.backend.service.ContactService;
 
 @Route("")
 public class MainView extends VerticalLayout {
+    private Grid<Contact> grid = new Grid<>(Contact.class);
+    private ContactService contactService;
 
-    public MainView() {
+    public MainView(final ContactService contactService) {
+        this.contactService = contactService;
+        addClassName("list-view");
+        setSizeFull();
+        configureGrid();
+        add(grid);
+        updateList();
+    }
 
+    private void configureGrid() {
+        grid.addClassName("contact-grid");
+        grid.setSizeFull();
+        grid.setColumns("firstName", "lastName", "email", "status");
+    }
+
+    private void updateList() {
+        grid.setItems(contactService.findAll());
     }
 
 }
