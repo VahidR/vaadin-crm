d761cd709611c6eab95f517f9095bb19e875c7f9
vahid <vahid.rafiei@gmail.com>
2021-05-30 17:26:23 +0200

    Making the ContactService more comprehensive and adding test data in the @PostConstruct method;

--- src/main/java/com/vaadin/tutorial/crm/backend/service/ContactService.java
+++ src/main/java/com/vaadin/tutorial/crm/backend/service/ContactService.java
@@ -1,11 +1,17 @@
 package com.vaadin.tutorial.crm.backend.service;
 
+import com.vaadin.tutorial.crm.backend.entity.Company;
 import com.vaadin.tutorial.crm.backend.entity.Contact;
+import com.vaadin.tutorial.crm.backend.repository.CompanyRepository;
 import com.vaadin.tutorial.crm.backend.repository.ContactRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by vahid
@@ -16,9 +22,11 @@ import java.util.List;
 @Slf4j
 public class ContactService {
     private ContactRepository contactRepository;
+    private CompanyRepository companyRepository;
 
-    public ContactService(final ContactRepository contactRepository) {
+    public ContactService(final ContactRepository contactRepository, final CompanyRepository companyRepository) {
         this.contactRepository = contactRepository;
+        this.companyRepository = companyRepository;
     }
 
     public List<Contact> findAll() {
@@ -39,6 +47,40 @@ public class ContactService {
             return;
         }
         contactRepository.save(contact);
+    }
+
+    @PostConstruct
+    public void populateTestData() {
+        if (companyRepository.count() == 0) {
+            companyRepository.saveAll(
+                    Stream.of("Path-Way Electronics", "E-Tech Management", "Path-E-Tech Management")
+                            .map(Company::new)
+                            .collect(Collectors.toList()));
+        }
+
+        if (contactRepository.count() == 0) {
+            Random r = new Random(0);
+            List<Company> companies = companyRepository.findAll();
+            contactRepository.saveAll(
+                    Stream.of("Gabrielle Patel", "Brian Robinson", "Eduardo Haugen",
+                            "Koen Johansen", "Alejandro Macdonald", "Angel Karlsson", "Yahir Gustavsson", "Haiden Svensson",
+                            "Emily Stewart", "Corinne Davis", "Ryann Davis", "Yurem Jackson", "Kelly Gustavsson",
+                            "Eileen Walker", "Katelyn Martin", "Israel Carlsson", "Quinn Hansson", "Makena Smith",
+                            "Danielle Watson", "Leland Harris", "Gunner Karlsen", "Jamar Olsson", "Lara Martin",
+                            "Ann Andersson", "Remington Andersson", "Rene Carlsson", "Elvis Olsen", "Solomon Olsen",
+                            "Jaydan Jackson", "Bernard Nilsen")
+                            .map(name -> {
+                                String[] split = name.split(" ");
+                                Contact contact = new Contact();
+                                contact.setFirstName(split[0]);
+                                contact.setLastName(split[1]);
+                                contact.setCompany(companies.get(r.nextInt(companies.size())));
+                                contact.setStatus(Contact.Status.values()[r.nextInt(Contact.Status.values().length)]);
+                                String email = (contact.getFirstName() + "." + contact.getLastName() + "@" + contact.getCompany().getName().replaceAll("[\\s-]", "") + ".com").toLowerCase();
+                                contact.setEmail(email);
+                                return contact;
+                            }).collect(Collectors.toList()));
+        }
     }
 
 }
